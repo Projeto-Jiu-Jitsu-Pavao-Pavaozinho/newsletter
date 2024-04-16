d6b0dbbb8573ce816a1682ae3f4ba1bf029aa291
Emanuel Wellyngton <emanuelwellygton@gmail.com>
2024-04-16 01:17:02 -0300

    update: NewsletterApplication

--- src/main/java/br/ong/pjjpavaozinho/newsletter/NewsletterApplication.java
+++ src/main/java/br/ong/pjjpavaozinho/newsletter/NewsletterApplication.java
@@ -1,21 +1,27 @@
 package br.ong.pjjpavaozinho.newsletter;
 
+import br.ong.pjjpavaozinho.newsletter.dto.EmailDTO;
+import br.ong.pjjpavaozinho.newsletter.entities.EmailEntity;
+import br.ong.pjjpavaozinho.newsletter.repositories.EmailRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
 public class NewsletterApplication {
+	private EmailRepository repository;
 
 	@GetMapping("/")
 	public String hello() {
 		return "Hello";
 	}
 
-	public void incricao(String email) {
-
+	@PostMapping("/")
+	@ResponseBody
+	public ResponseEntity incricao(@RequestBody EmailDTO request) {
+		return ResponseEntity.ok(new EmailEntity(request.getEmail()));
 	}
 
 	public static void main(String[] args) {
