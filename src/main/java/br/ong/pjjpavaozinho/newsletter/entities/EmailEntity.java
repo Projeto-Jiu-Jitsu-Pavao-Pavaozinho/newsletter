4128a2441d86cee562c01de5b67ecb8ff22aadc6
Emanuel Wellyngton <emanuelwellygton@gmail.com>
2024-04-16 01:17:34 -0300

    feat: EmailEntity

---
+++ src/main/java/br/ong/pjjpavaozinho/newsletter/entities/EmailEntity.java
@@ -0,0 +1,40 @@
+package br.ong.pjjpavaozinho.newsletter.entities;
+
+import jakarta.persistence.*;
+
+@Entity(name = "emails")
+public class EmailEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String email;
+
+    public EmailEntity() {
+    }
+
+    public EmailEntity(int id, String email) {
+        this.id = id;
+        this.email = email;
+    }
+
+    public EmailEntity(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
