3e9f91c473e471a3ff093d28a7664b3f4e30d5a9
Emanuel Wellyngton <emanuelwellygton@gmail.com>
2024-04-18 05:45:18 -0300

    fix: id gerado como identidade

--- src/main/java/br/ong/pjjpavaozinho/newsletter/entities/EmailEntity.java
+++ src/main/java/br/ong/pjjpavaozinho/newsletter/entities/EmailEntity.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 public class EmailEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String email;
 
